342af56d3c9845a6bed03da13e382a7f2199a637
Joseph Hughes <hughes.joseph@gmail.com>
2020-05-13 18:41:20 +0100

    Script to prune a tree to only keep tips that have a particular name

---
+++ Prune.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# python Prune.py --tree ~/Documents/Coronavirus/cog20200508/cog_global_2020-05-08_tree.newick --keyword Scotland --out Scottish_ncov.tre
+
+import argparse
+from ete3 import Tree
+import sys
+import re
+
+# Loads a tree structure from a newick file
+parser = argparse.ArgumentParser(description="Prune a tree keeping tip labels that contain a particular string", add_help=False)
+parser.add_argument("--tree", help="The newick tree file.\n")
+parser.add_argument("--keyword", help="Keyword to look for in the tip label.\n")
+parser.add_argument("--out", help="The output pruned tree in newick format.\n")
+parser.add_argument("-h","--help", action="store_true")
+args = parser.parse_args()
+
+def custom_help():
+    print("Required Arguments!!\n\t--tree [file.newick]\n")
+    print("\t--keyword [text]\n")
+    print("\t--out [output.newick]\n")
+
+if len(sys.argv) == 3 or args.help:
+  print(custom_help())
+  exit(1)
+
+with open(args.tree, 'r') as file:
+    data = file.read().replace('\n', '')
+
+data=data.replace("[&R] ","")
+t = Tree(data)
+
+keep=0;
+for leaf in t:
+  # Do some analysis on node
+  #print(node.name)
+  if args.keyword in leaf.name:
+    print(leaf.name)
+    keep+=1
+  else:
+    leaf.delete()
+print("Keeping ",keep," sequences")
+
+t.write(format=1, outfile=args.out)
